e203317f948da37c0f328609195275ebeddfa3fd
Martin <martin.stankard@hotmail.com>
2021-01-07 08:23:16 -0700

    there we go

--- src/components/counter-display.js
+++ src/components/counter-display.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Statistic } from "semantic-ui-react";
-import { CounterContext } from "./counter-context";
+import { CounterContext } from "../context/counter-context";
 
 export default function CounterDisplay() {
   const [count] = useContext(CounterContext);
