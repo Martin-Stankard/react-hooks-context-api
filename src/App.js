568d05e6b876ceb186ea3904a48f1496edbd507f
Martin <martin.stankard@hotmail.com>
2021-01-07 16:57:19 -0700

    contact table view works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
-
-import CounterView from "./views/counter-view";
+import ContactView from "./views/contact-view";
 
 export default function App() {
   return (
     <Container>
       <h1>React Hooks Context Demo</h1>
-      <CounterView />
+      <ContactView />
     </Container>
   );
 }
