28df3282de26342790c6a70100d7f2ccb3d45ad9
Martin <martin.stankard@hotmail.com>
2021-01-07 17:02:43 -0700

    both views in place and state appears to move betwixt components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
-import ContactView from "./views/contact-view";
+
+import CounterView from "./views/counter-view";
+import ContactView from "./views/contact-view"
 
 export default function App() {
   return (
     <Container>
       <h1>React Hooks Context Demo</h1>
-      <ContactView />
+      <CounterView />
+      <ContactView/>
     </Container>
   );
 }
